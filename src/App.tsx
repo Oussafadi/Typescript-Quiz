a107d8e22d0dfd32b9394937c90dc511e239a01f
Oussafadi <fadi.oussa@outlook.fr>
2024-01-10 20:37:23 +0100

    finish i will refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { EventHandler, FormEventHandler, useState } from 'react'
 import './App.css'
 
+type Question = {
+  question :string,
+  answer: string,
+  options : string[],
+}
+
+const questions : Question[] =
+[{
+  question : "Capital of Morocco ?",
+  answer: "Rabat",
+  options : [
+    'Rabat',
+    'Casablanca',
+    'Marrakech'
+  ]
+},
+{
+  question : "Capital of France ?",
+  answer: "Paris",
+  options : [
+    'Paris',
+    'Toulouse',
+    'Montpellier'
+  ]
+},
+{
+  question : "Capital of the USA ?",
+  answer: "Washignton",
+  options : [
+    'California',
+    'Ottawa',
+    'Washignton'
+  ]
+}
+]
+
+
 function App() {
-  const [count, setCount] = useState(0)
+ const [currentQuestionNumber,setCurrentQuestionNumber] = useState<number>(0);
+ const [answer,setAnswer] = useState<string>('');
+ const [score,setScore ] = useState<number>(0);
+ const [message,setMessage] = useState<string>('');
+ 
+ const isGameOver = currentQuestionNumber>= questions.length;
+
+
+ // Computed Values
+ const currentQuestion =questions[currentQuestionNumber];
+ const wrongAnswers = questions.length - score ;
+ 
+
+const handleSubmit = (e : any) => {
+   e.preventDefault();
+   
+    if (answer === currentQuestion.answer) {
+      setScore((score) => score +1);
+      if(score == questions.length) {
+        setMessage("Congratualtion you won");
+      }
+    }
+    setCurrentQuestionNumber((currentQuestionNumber) => currentQuestionNumber +1) ;
+
+    
+}
+
+function Quiz() {
+ return ( <div className='quiz'>
+  <h1 className='question'>
+      { currentQuestion.question }
+  </h1>
+  <form className='form-options' onSubmit={handleSubmit}>
+  <div >
+      {currentQuestion.options.map(o => 
+        (
+          <div className='options'  key={o}>
+          <input type='radio' onChange={() => {setAnswer(o)}} value={o} name='answer' checked={o == answer }>
+          </input>
+          {o}
+          </div>
+        ))}
+  </div>
+  <button>Submit Answer</button>
+  </form>
+</div>
+ )
+}
+
+function ScoreComponent() {
+  return (
+    <>
+       { score == questions.length ?  
+       (
+          <h2> {message} </h2>
+       ) 
+          :
+         (
+          <h2>  You got {wrongAnswers}  wrong </h2>
+           )
+       }
+      
+      </>
+  )
+}
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    { isGameOver ? 
+    (
+      <ScoreComponent/>
+    )
+    :
+    (
+     <Quiz/>
+    )
+    }
+      
+    
     </>
   )
 }
